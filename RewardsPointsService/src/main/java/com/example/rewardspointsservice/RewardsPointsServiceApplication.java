238070977536d0d19e0759c4024aa31e2a31a82d
suikax86 <suikax86@outlook.com>
2024-07-27 13:56:47 +0700

    init RewardsPointsService

---
+++ RewardsPointsService/src/main/java/com/example/rewardspointsservice/RewardsPointsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rewardspointsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardsPointsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardsPointsServiceApplication.class, args);
+    }
+
+}
