cf01db8812993b7d5ed4c874e40de2a04d960e51
suikax86 <suikax86@outlook.com>
2024-08-22 11:50:04 +0700

    (front-end) Employee Profile

--- HRM-Client/src/pages/MyProfile/MyProfile.jsx
+++ HRM-Client/src/pages/MyProfile/MyProfile.jsx
@@ -1,10 +1,16 @@
-import React from "react";
 import ProfileForm from "../../components/ProfileForm/ProfileForm";
+import Header from "../../components/common/Header/Header.jsx";
+import Footer from "../../components/common/Footer/Footer.jsx";
+import "./MyProfile.scss";
 
 function MyProfile() {
   return (
-    <div>
-      <ProfileForm></ProfileForm>
+    <div className="my-profile-container">
+      <Header />
+      <div className="content">
+        <ProfileForm />
+      </div>
+      <Footer />
     </div>
   );
 }
