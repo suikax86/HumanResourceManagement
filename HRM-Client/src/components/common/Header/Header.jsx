05677f5c8f19a848031abad4c7b61a95bb743a87
suikax86 <suikax86@outlook.com>
2024-08-25 19:38:25 +0700

    (back-end) Configure CORS, (front-end) make http request for login page and employee profile, loading spinner

--- HRM-Client/src/components/common/Header/Header.jsx
+++ HRM-Client/src/components/common/Header/Header.jsx
@@ -19,7 +19,9 @@ function Header() {
         </NavLink>
         
       </div>
-      <button className="header_container_button">Log in</button>
+      <NavLink to="/login">
+        <button className="header_container_button">Log in</button>
+      </NavLink>
     </div>
   );
 }
