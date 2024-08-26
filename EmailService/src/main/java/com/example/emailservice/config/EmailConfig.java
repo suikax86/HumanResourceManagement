ea7b3bd287390c5398e6dd57235024a26f4e1f7f
suikax86 <suikax86@outlook.com>
2024-08-26 08:15:39 +0700

    rabbitmq configuration for email service

---
+++ EmailService/src/main/java/com/example/emailservice/config/EmailConfig.java
@@ -0,0 +1,28 @@
+package com.example.emailservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("email-smtp.ap-southeast-1.amazonaws.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("REMOVED");
+        mailSender.setPassword("REMOVED+TLrrgSY");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+
+        return mailSender;
+    }
+}
