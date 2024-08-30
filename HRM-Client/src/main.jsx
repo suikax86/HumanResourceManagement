c1070b3eedd93caace9ccc7bf57c4d910d924d8e
suikax86 <suikax86@outlook.com>
2024-08-30 23:58:58 +0700

    nothing change

--- HRM-Client/src/main.jsx
+++ HRM-Client/src/main.jsx
@@ -6,7 +6,6 @@ import Login from './pages/Login/Login.jsx'
 import MyProfile from './pages/MyProfile/MyProfile.jsx';
 import DonXinNghi from './pages/DonXinNghi/Don.jsx';
 import DuyetDon from './pages/DuyetDon/DuyetDon.jsx';
-import WorkFromHome from './pages/WorkFromHome/WorkHome.jsx';
 import VoucherPage from './pages/VoucherPage/VoucherPage.jsx';
 import HistoryApplication from './pages/HistoryApplication/History.jsx';
 
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route exac path="/HomePage" element={<HomePage />} />
         <Route exac path="/MyProfile" element={<MyProfile />} />
         <Route exac path="/DonXinNghi" element={<DonXinNghi />} />
-        <Route exac path="/WorkFromHome" element={<WorkFromHome />} />
         <Route exac path="/Voucher" element={<VoucherPage />} />
         <Route exac path="/DonXinNghi" element={<DonXinNghi />} />
         <Route exac path="/HistoryApplication" element={<HistoryApplication />} />
