7293f27745a6d39a27a9bcf99f56e4593d7d2c09
suikax86 <suikax86@outlook.com>
2024-09-04 19:54:11 +0700

    CheckInCheckOut page

--- HRM-Client/src/pages/VoucherPage/VoucherPage.jsx
+++ HRM-Client/src/pages/VoucherPage/VoucherPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import './VoucherPage.scss'
 import Header from '../../components/common/Header/Header'
 import Footer from '../../components/common/Footer/Footer'
 import VoucherList from '../../components/VoucherList/VoucherList'
 
 function VoucherPage() {
   return (
-    <div className='VoucherPage_Container'>
-      <Header/>
-      <VoucherList/>
-      <Footer/>
+    <div className="my-profile-container">
+      <Header />
+      <div className="content">
+        <VoucherList />
+      </div>
+      <Footer />
     </div>
   )
 }
