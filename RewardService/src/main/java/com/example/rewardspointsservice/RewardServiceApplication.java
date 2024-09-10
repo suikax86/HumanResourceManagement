8d7a971bca23fe8d25d1dce8cff060796456e121
suikax86 <suikax86@outlook.com>
2024-09-10 10:32:00 +0700

    change strava service -> activity service

---
+++ RewardService/src/main/java/com/example/rewardspointsservice/RewardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rewardspointsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardServiceApplication.class, args);
+    }
+
+}
---
+++ RewardService/src/main/java/com/example/rewardspointsservice/controller/RewardPointsProfileController.java
@@ -0,0 +1,35 @@
+package com.example.rewardspointsservice.controller;
+
+import com.example.rewardspointsservice.model.dtos.RewardPointsProfileDto;
+import com.example.rewardspointsservice.service.RewardPointsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rewards")
+public class RewardPointsProfileController {
+    private final RewardPointsService rewardPointsService;
+
+    public RewardPointsProfileController(RewardPointsService rewardPointsService) {
+        this.rewardPointsService = rewardPointsService;
+    }
+
+    @GetMapping("/{employeeId}")
+    public RewardPointsProfileDto getRewardPoints(@PathVariable Long employeeId) {
+        return rewardPointsService.getProfileDto(employeeId);
+    }
+
+    @GetMapping("/all")
+    public List<RewardPointsProfileDto> getAllRewardPoints() {
+       return rewardPointsService.getAllProfilesDto();
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createRewardPointsProfile(@RequestBody Long employeeId, String employeeName) {
+        String response = rewardPointsService.createRewardPointsProfile(employeeId, employeeName);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
